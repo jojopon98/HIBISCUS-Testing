9e7dd6b5c783c2aa3d609e608606af1aab3f8271
perfectharmonii <oai4@njit.edu>
2019-11-07 18:04:08 -0500

    Fixed Oculus controller.

---
+++ Assets/Assets/Scripts/SkyboxSwap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxSwap : MonoBehaviour
+{
+
+    public Material skybox1, skybox2, skybox3, skybox4, skybox5;
+
+    // Use this for initialization
+    void Start()
+    {
+        RenderSettings.skybox = skybox1;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Red"))
+        {
+            RenderSettings.skybox = skybox1;
+        }
+        else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Orange"))
+        {
+            RenderSettings.skybox = skybox2;
+        }
+        else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Yellow"))
+        {
+            RenderSettings.skybox = skybox3;
+        }
+        else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Green"))
+        {
+            RenderSettings.skybox = skybox4;
+        }
+        else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Blue"))
+        {
+            RenderSettings.skybox = skybox5;
+        }
+    }
+}
